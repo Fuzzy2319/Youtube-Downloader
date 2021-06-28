33050081b7cae5d2c448ecea36f8d080101db144
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2021-06-28 10:39:44 +0200

    Update dependances & Fix download message

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const Fs = require("fs");
 const Ytdl = require("ytdl-core");
 
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 if (args.length === 2) {
     if (Ytdl.validateURL(args[0])) {
         Ytdl(args[0], {
             quality: "highest",
-        }).pipe(Fs.createWriteStream(args[1] + ".mp4"));
-        console.log("Download complete and saved as " + args[1] + ".mp4");
+        }).pipe(Fs.createWriteStream(args[1] + ".mp4").on("finish", () => {
+            console.log("Download complete and saved as " + args[1] + ".mp4");
+        }));
     } else {
         console.log(args[0] + " isn't a valid youtube's video url");
     }
